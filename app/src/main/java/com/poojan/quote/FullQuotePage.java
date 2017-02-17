6afaff2db8ba52a26ae4a771fc742f7167f7afdc
PoojanParikh <camkampar@gmail.com>
2017-02-17 10:53:47 +0530

    Full Quote Page class activity_full_quote_page xml is Created

---
+++ app/src/main/java/com/poojan/quote/FullQuotePage.java
@@ -0,0 +1,18 @@
+package com.poojan.quote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by VNurtureTechnologies on 17/02/17.
+ */
+
+public class FullQuotePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_quote_page);
+    }
+
+}
