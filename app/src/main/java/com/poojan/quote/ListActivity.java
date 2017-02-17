bf8942fa7c2c857820c85263b3098e0f05796b5c
PoojanParikh <camkampar@gmail.com>
2017-02-17 10:46:01 +0530

    List Activity class and activity_list_main is Created

---
+++ app/src/main/java/com/poojan/quote/ListActivity.java
@@ -0,0 +1,18 @@
+package com.poojan.quote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by VNurtureTechnologies on 17/02/17.
+ */
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_main);
+    }
+
+}
