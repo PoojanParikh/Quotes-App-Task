3fc72b227ebce5ff0116089fc9596f24bcc14847
PoojanParikh <camkampar@gmail.com>
2017-02-17 13:03:53 +0530

    Changed Share Button from Full Quote Page to Menu

--- app/src/main/java/com/poojan/quote/FullQuotePage.java
+++ app/src/main/java/com/poojan/quote/FullQuotePage.java
@@ -3,6 +3,8 @@ package com.poojan.quote;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,7 +27,7 @@ public class FullQuotePage extends AppCompatActivity {
 
         quoteText.setText(quote);
 
-        shareImage = (ImageView) findViewById(R.id.share_image);
+        /*shareImage = (ImageView) findViewById(R.id.share_image);
 
         shareImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +40,40 @@ public class FullQuotePage extends AppCompatActivity {
                 shareIntent.putExtra(Intent.EXTRA_TEXT, quotes);
                 startActivity(Intent.createChooser(shareIntent, "Share using"));
             }
-        });
+        });*/
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.action_bar_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.share_icon: {
+                /*Share.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {*/
+                        Intent shareIntent = new Intent();
+
+                        shareIntent.setType("text/plain");
+                        Intent intent = getIntent();
+                        final String quotes = intent.getStringExtra("Quotes");
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, quotes);
+                        startActivity(Intent.createChooser(shareIntent, "Share using"));
+                   /* }
+                });*/
+                return true;
+            }
+
+    }
+
+        return super.onOptionsItemSelected(item);
+}
 
 }
