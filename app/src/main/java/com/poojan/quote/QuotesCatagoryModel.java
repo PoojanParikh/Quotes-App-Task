9a94b2cf1f40c2d0e03d3d08a6d6bfdef1ab4494
PoojanParikh <camkampar@gmail.com>
2017-02-17 10:41:24 +0530

    Quotes Catagory Model is Created

---
+++ app/src/main/java/com/poojan/quote/QuotesCatagoryModel.java
@@ -0,0 +1,29 @@
+package com.poojan.quote;
+
+/**
+ * Created by VNurtureTechnologies on 17/02/17.
+ */
+
+public class QuotesCatagoryModel {
+
+    public String getCatagory() {
+        return catagory;
+    }
+
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
+    }
+
+    String catagory;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    int id;
+
+}
